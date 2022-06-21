cbe2ccbf0223e21bbecabaf79c48fcbcf6590aa2
duanpk <duanpk@pascaliaasia.com>
2022-06-21 11:29:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+# Include header
+require_once __DIR__ . '/modules/partials/header.php';
+# Include main contain
+require_once __DIR__ . '/modules/home.php';
+# Include footer
+require_once __DIR__ . '/modules/partials/footer.php';
---
+++ modules/home.php
@@ -0,0 +1,17 @@
+<head>
+    <link rel="stylesheet" href="normalize.css">
+    <meta charset="utf-8">
+    <title>My web page</title>
+    <link rel="stylesheet" href="../assets/css/index.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <script src="../assets/js/index.js"></script>
+</head>
+<!-- MAIN content -->
+<div id="main">
+    <div id="main-content">
+        <h3> The main content go here</h3>
+    </div>
+    <!-- embed sidebar.php -->
+    <?php require_once __DIR__ . '/partials/sidebar.php' ?>
+</div>
